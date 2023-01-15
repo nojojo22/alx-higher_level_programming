973e56dc5e8784801dc6f1516173e99c08c64ba7
Onojo Ameh <amehonojo@gmail.com>
2023-01-14 16:18:58 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for num in range(99)
-    print("{:d} = {:s}".format(num, hex(num)))
+for char in range(26):
+    if char != 4 and char != 16:
+        print("{:s}".format(chr(char + ord("a"))), end="")
